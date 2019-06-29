86a060c5c60c98f0449a9c4c8834190395619fb9
kobakakzu0429 <kobakazu10@gmail.com>
2019-06-30 00:12:25 +0900

    record

--- src/Pages/Shiritori.tsx
+++ src/Pages/Shiritori.tsx
@@ -9,17 +9,34 @@ export default ({ history, match }: RouteComponentProps<IShiritori>) => {
   const { params } = match;
   const { keyword } = params;
 
+  const [answered, setAnswered] = React.useState<number[]>([]);
+
   const handleClick = React.useCallback(
-    (name: string) => (_: React.MouseEvent<HTMLLIElement, MouseEvent>) => {
+    (name: string, no: number) => (
+      _: React.MouseEvent<HTMLLIElement, MouseEvent>
+    ) => {
+      if (isAnswered(no)) return;
       history.push(`/shiritori/${lastName(name)}`);
+      setAnswered([...answered, no]);
     },
-    []
+    [answered]
   );
+
+  const isAnswered = (no: number) =>
+    answered.find(element => element === no) ? true : false;
 
   return (
     <ul>
       {PokemonLists[keyword].map(pokemon => (
-        <li key={pokemon.no} onClick={handleClick(pokemon.name)}>
+        <li
+          key={pokemon.no}
+          onClick={handleClick(pokemon.name, pokemon.no)}
+          style={
+            isAnswered(pokemon.no)
+              ? { textDecoration: "line-through" }
+              : undefined
+          }
+        >
           {pokemon.name}
         </li>
       ))}
